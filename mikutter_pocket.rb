e70d8279cb7dbecc1eabb9e74ffd6c25d48b1cb4
yuzumone <yuzumone@outlook.com>
2017-01-24 13:11:34 +0900

    書き換えた時Thread忘れた

--- mikutter_pocket.rb
+++ mikutter_pocket.rb
@@ -2,71 +2,81 @@
 
 Plugin.create(:mikutter_pocket) do
 
+  defactivity "pocket", "pocket"
   defevent :pocket_request
   defevent :pocket_authorize
   defevent :pocket_add, prototype: [Message]
 
   # Pocketの認証のためのコードをリクエストするイベント
   on_pocket_request do
-    uri = URI.parse('https://getpocket.com/v3/oauth/request')
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req["Content-Type"] = "application/json"
-    req["X-Accept"] = "application/json"
-    data = {
-      "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
-      "redirect_uri" => "https://www.google.com"
-    }.to_json
-    req.body = data
-    res = https.request(req)
-    params = JSON.parse(res.body)
-    UserConfig[:mikutter_pocket_code] = params["code"]
-    url = "https://getpocket.com/auth/authorize?request_token=" +
-          params["code"] + "&redirect_uri=https://www.google.com"
-    ::Gtk::TimeLine.openurl(url)
+    Thread.new {
+      uri = URI.parse('https://getpocket.com/v3/oauth/request')
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req["Content-Type"] = "application/json"
+      req["X-Accept"] = "application/json"
+      data = {
+        "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
+        "redirect_uri" => "https://www.google.com"
+      }.to_json
+      req.body = data
+      res = https.request(req)
+      params = JSON.parse(res.body)
+      UserConfig[:mikutter_pocket_code] = params["code"]
+      "https://getpocket.com/auth/authorize?request_token=" +
+        params["code"] + "&redirect_uri=https://www.google.com"
+    }.next { |url|
+      Gtk.openurl(url)
+    }.trap{ |e| error e }
   end
 
   # Pocketの認証をするイベント
   on_pocket_authorize do
-    uri = URI.parse('https://getpocket.com/v3/oauth/authorize')
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req["Content-Type"] = "application/json"
-    req["X-Accept"] = "application/json"
-    data = {
-      "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
-      "code" => UserConfig[:mikutter_pocket_code]
-    }.to_json
-    req.body = data
-    res = https.request(req)
-    params = JSON.parse(res.body)
-    UserConfig[:mikutter_pocket_token] = params["access_token"]
-    UserConfig[:mikutter_pocket_code] = nil
+    Thread.new {
+      uri = URI.parse('https://getpocket.com/v3/oauth/authorize')
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req["Content-Type"] = "application/json"
+      req["X-Accept"] = "application/json"
+      data = {
+        "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
+        "code" => UserConfig[:mikutter_pocket_code]
+      }.to_json
+      req.body = data
+      res = https.request(req)
+      params = JSON.parse(res.body)
+      params["access_token"]
+    }.next { |token|
+      UserConfig[:mikutter_pocket_token] = token
+      UserConfig[:mikutter_pocket_code] = nil
+    }.trap{ |e| error e }
   end
 
   # Pocketに追加するイベント
   on_pocket_add do |msg|
-    url = "https://twitter.com/#{msg.user[:idname]}/status/#{msg.id}"
-    uri = URI.parse('https://getpocket.com/v3/add')
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req["Content-Type"] = "application/json"
-    req["X-Accept"] = "application/json"
-    data = {
-      "url" => url,
-      "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
-      "access_token" => UserConfig[:mikutter_pocket_token]
-    }.to_json
-    req.body = data
-    res = https.request(req)
-    if res.code == '200'
-      activity(:system, "#{url}をPocketしました")
-    else
-      activity(:system, "#{url}をPocketできませんでした")
-    end
+    Thread.new {
+      url = "https://twitter.com/#{msg.user[:idname]}/status/#{msg.id}"
+      uri = URI.parse('https://getpocket.com/v3/add')
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req["Content-Type"] = "application/json"
+      req["X-Accept"] = "application/json"
+      data = {
+        "url" => url,
+        "consumer_key" => "48185-0b1cb79c6eaf08bb2b5a6554",
+        "access_token" => UserConfig[:mikutter_pocket_token]
+      }.to_json
+      req.body = data
+      res = https.request(req)
+      if res.code == '200'
+        activity(:pocket, "#{url}をPocketしました")
+      else
+        activity(:pocket, "#{url}をPocketできませんでした")
+      end
+    }.trap { |e| error e }
   end
 
   # PocketのTokenを持っているなら真
